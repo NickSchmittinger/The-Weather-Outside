b038bfe12f8452c94dc4f18c838dada6438fad4b
Nick Schmittinger <nicholasschmittinger@gmail.com>
2024-07-28 15:13:37 -0400

    From Desktop 1
    Reset of general design.
    
    Changed:
    - Javascript functionality to access 2 Api services
    - HTML for drop down divs and section
    - CSS styling for drop down divs
    - CSS styling for output data section
    
    To be added:
    + Removal of geo Api calls replaced with static lat / lon values per location
    +Potential addition of search bar to allow incorporation of geo Api

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const searchButton = document.getElementById('search-button');
-    const searchCity = document.getElementById('search-city');
-    const weatherInfo = document.getElementById('weather-info');
+    const weatherData = document.getElementById('weather-data');
+    const locations = document.querySelectorAll('.dropdown-content a');
+    const weatherApiKey = 'Not in use';
+    const weatherApiUrl = 'https://api.openweathermap.org/data/3.0/onecall';
 
-    searchCity.addEventListener('keydown', (event) =>{
-        if(event.key == 'Enter'){
-            const weatherHTML = `<h2>TEST</h2>`;
-            weatherInfo.innerHTML = weatherHTML;
-        }
-    });
+    //This will most likely be removed unless search functionality implemented.
+    const geoApiKey = 'Not in use';
+    const groApiUrl = 'https://api.openweathermap.org/geo/1.0/direct'
 
-    searchButton.addEventListener('click', () => {
-        const city = searchCity.ariaValueMax;
-
-        const weatherHTML = `<h2>${city}</h2>
-                                <p>Temp: -- C</p>
-                                <p>Weather: --</p>
-                                <p>Humidity --%</p>
-                                <p>Wind Speed: -- m/s</p>
-                                `;
-        
-        weatherInfo.innerHTML = weatherHTML;
+    locations.forEach(place => {
+        place.addEventListener('click', (event) =>{
+            event.preventDefault();
+            const location = place.getAttribute('data-location');
+            let lat = 0.0;
+            let lon = 0.0;
+            fetch(`${groApiUrl}?q=${location}&limit=5&appid=${geoApiKey}`)
+                .then(response => response.json())
+                .then(data => {
+                    lat = data[0].lat;
+                    lon = data[0].lon;
+                })
+                .catch(error => {
+                    console.error('Could not read geo location data:', error);
+                });
+            fetch(`${weatherApiUrl}?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`)
+                .then(response => response.json())
+                .then(data => {
+                    const newHTML = `
+                        <h2>${location}</h2>
+                        <p>Weather: ${data[0].current.weather[0].main}</p>
+                        <p>Temperature: ${data[0].current.temp}°C</p>
+                        <p>Humidity: ${data[0].current.humidity}%</p>
+                    `;
+                    weatherData.innerHTML = newHTML;
+                })
+                .catch(error => {
+                    weatherData.innerHTML = `<p>Error getting data.</p>`;
+                    console.error('Error detected regarding the data collection process:', error);
+                });
+        });
     });
 });
